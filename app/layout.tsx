79d50cb972e5068392227e2e169356338387aebd
Tobias <10495193+schmuecker@users.noreply.github.com>
2025-08-26 18:15:25 +0200

    Import Script component from next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script';
 import './globals.css'
 import ClientWrapper from './client-wrapper';
 
